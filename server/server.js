f2f64adfd9587fada71912ba96535635612c0c5c
Paul David Tan <cryxuz@gmail.com>
2023-08-01 16:46:13 +1200

    need test

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import * as Path from 'node:path/posix'
 import * as URL from 'node:url'
 import express from 'express'
 import hbs from 'express-handlebars'
+import art from './data/art.js'
 
 const server = express()
 export default server
@@ -22,3 +23,24 @@ server.set('views', __dirname + '/views')
 server.use(express.static(__dirname + '/public'))
 
 // Routes
+
+server.get('/', (req, res) => {
+  const viewData = {
+    title: 'Gallery',
+    art,
+  }
+
+  res.render('home', viewData)
+})
+
+server.get('/artworks/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const artwork = art.find((art) => art.id === id)
+
+  const viewData = {
+    art,
+    ...artwork,
+  }
+
+  res.render('artworks', viewData)
+})
